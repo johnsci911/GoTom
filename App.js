d9cfb15d35908a64e889047d94936af062557e58
jkcachero <johnguitarizta@gmail.com>
2024-06-17 21:01:28 +0800

    Format and fix deprecated useProps

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant.screen";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
-import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,30 +1,6 @@
 import { View } from "react-native";
 import styled, { useTheme } from "styled-components/native";
 
-const TopSmall = styled(View)`
-  margin-top: 4px;
-`;
-
-const TopMedium = styled(View)`
-  margin-top: 8px;
-`;
-
-const TopLarge = styled(View)`
-  margin-top: 16px;
-`;
-
-const LeftSmall = styled(View)`
-  margin-left: 4px;
-`;
-
-const LeftMedium = styled(View)`
-  margin-left: 8px;
-`;
-
-const LeftLarge = styled(View)`
-  margin-left: 16px;
-`;
-
 const sizeVariant = {
   small: 1,
   medium: 2,
@@ -44,7 +20,7 @@ const getVariant = (position, size, theme) => {
   const value = theme.space[sizeIndex];
 
   `${property}:${value}`;
-}
+};
 
 const SpacerView = styled(View)`
   ${({ variant }) => variant};
@@ -54,14 +30,10 @@ export const Spacer = ({ position, size, children }) => {
   const theme = useTheme();
   const variant = getVariant(position, size, theme);
 
-  return (
-    <SpacerView variant={variant}>
-      {children}
-    </SpacerView>
-  )
+  return <SpacerView variant={variant}>{children}</SpacerView>;
 };
 
-Spacer.defaultProps = {
+Spacer.default = {
   position: "top",
   size: "small",
 };
