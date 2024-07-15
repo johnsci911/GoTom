a8eaf98f8f7455374b8ca2fdf361ad8eb7d29c17
jkcachero <johnguitarizta@gmail.com>
2024-07-15 09:25:20 +0800

    Search bar only trigger after hitting enter key

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,6 +1,17 @@
-import React, { useState, useEffect, createContext, useMemo } from "react";
+import React, {
+  useState,
+  useContext,
+  createContext,
+  useEffect,
+  useMemo,
+} from "react";
 
-import { restaurantsRequest, restaurantsTransform } from "./restaurants.service"
+import {
+  restaurantsRequest,
+  restaurantsTransform,
+} from "./restaurants.service";
+
+import { LocationContext } from "../location/location.context";
 
 export const RestaurantsContext = createContext();
 
@@ -8,35 +19,41 @@ export const RestaurantsContextProvider = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { location } = useContext(LocationContext);
 
-  const retrieveRestaurants = () => {
+  const retrieveRestaurants = (loc) => {
     setIsLoading(true);
-    setTimeout(() => {
-      restaurantsRequest()
-        .then(restaurantsTransform)
-        .then((restaurants) => {
-          setRestaurants(restaurants);
-          setIsLoading(false);
-        }).catch((error) => {
-          setError(error);
-          setIsLoading(false);
-        });
-    }, 2000)
-  }
+    setRestaurants([]);
 
+    setTimeout(() => {
+      restaurantsRequest(loc)
+        .then(restaurantsTransform)
+        .then((results) => {
+          setIsLoading(false);
+          setRestaurants(results);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err);
+        });
+    }, 2000);
+  };
   useEffect(() => {
-    retrieveRestaurants();
-  }, []);
+    if (location) {
+      const locationString = `${location.lat},${location.lng}`;
+      retrieveRestaurants(locationString);
+    }
+  }, [location]);
 
   return (
     <RestaurantsContext.Provider
       value={{
         restaurants,
         isLoading,
-        error
+        error,
       }}
     >
       {children}
     </RestaurantsContext.Provider>
-  )
-}
+  );
+};
